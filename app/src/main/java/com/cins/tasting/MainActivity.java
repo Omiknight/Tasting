d3e10acafb418f6547454de3bc0dc8268f09356b
jianping.jin <jianping.jin@ele.me>
2017-10-07 15:05:50 +0800

    base

--- app/src/main/java/com/cins/tasting/MainActivity.java
+++ app/src/main/java/com/cins/tasting/MainActivity.java
@@ -2,12 +2,32 @@ package com.cins.tasting;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+  }
+
+  @Override public void onClick(View view) {
+    switch (view.getId()) {
+      //基本组件
+      case R.id.bt_textview:
+
+        break;
+      case R.id.bt_imageview:
+        break;
+      case R.id.bt_viewpage:
+        break;
+      case R.id.bt_dialog:
+        break;
+      case R.id.bt_toast:
+        break;
+      case R.id.bt_checkbox:
+        break;
+    }
   }
 }
