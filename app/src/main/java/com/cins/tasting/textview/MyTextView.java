8d7737537bddd9767fa5f609e3ca3c2895f554b2
jianping.jin <jianping.jin@ele.me>
2017-10-10 18:57:42 +0800

    txt

--- app/src/main/java/com/cins/tasting/textview/MyTextView.java
+++ app/src/main/java/com/cins/tasting/textview/MyTextView.java
@@ -3,7 +3,11 @@ package com.cins.tasting.textview;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import com.cins.tasting.R;
+import com.cins.tasting.R2;
 
 /**
  * Created by jinjianping on 2017/10/7.
@@ -11,8 +15,13 @@ import com.cins.tasting.R;
 
 public class MyTextView extends AppCompatActivity {
 
+  @BindView(R2.id.tv_another)public TextView textView;
+
   @Override protected void onCreate(@Nullable Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-    setContentView(R.layout.texrview);
+    setContentView(R.layout.textview);
+    ButterKnife.bind(this);
+
+    textView.setCompoundDrawablesWithIntrinsicBounds(R.mipmap.ic_launcher,0,0,0);
   }
 }
